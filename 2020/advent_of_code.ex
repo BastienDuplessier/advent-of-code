09413d6fbbc1a0e3a3a0e13ccc22460ac3a3a5bf
Bastien Duplessier <bastien.duplessier@gmail.com>
2020-12-03 10:54:51 +0100

    day 2
    Signed-off-by: Bastien Duplessier <bastien.duplessier@wttj.co>

--- 2020/advent_of_code.ex
+++ 2020/advent_of_code.ex
@@ -1,4 +1,43 @@
 defmodule AdventOfCode do
+  # Imports <+>
+  import MyOperators
+
+  def problem_2 do
+    rx = ~r/(\d+)\-(\d+) (\w): (\w+)/
+
+    input =
+      rx
+      |> Regex.scan(File.read!("./assets/2"))
+
+    Enum.reduce(input, 0, fn [_, min, max, l, pwd], acc ->
+      [code] = String.to_charlist(l)
+      count = pwd |> String.to_charlist() |> Enum.filter(&(&1 == code)) |> Enum.count()
+
+      case count >= String.to_integer(min) && count <= String.to_integer(max) do
+        true -> acc + 1
+        false -> acc
+      end
+    end)
+  end
+
+  def problem_2_bis do
+    rx = ~r/(\d+)\-(\d+) (\w): (\w+)/
+
+    input =
+      rx
+      |> Regex.scan(File.read!("./assets/2"))
+
+    Enum.reduce(input, 0, fn [_, sa, sb, l, pwd], acc ->
+      a = String.to_integer(sa) - 1
+      b = String.to_integer(sb) - 1
+
+      case l == String.at(pwd, a) != (l == String.at(pwd, b)) do
+        true -> acc + 1
+        false -> acc
+      end
+    end)
+  end
+
   def problem_1 do
     numbers =
       File.read!("./assets/1")
