2b677365cb057e422a9af543dbbf2b5a5710ec8e
Bastien Duplessier <bastien.duplessier@gmail.com>
2021-12-06 11:42:04 +0100

    day 2
    Signed-off-by: Bastien Duplessier <bastien.duplessier@gmail.com>

--- advent_2021/src/main.rs
+++ advent_2021/src/main.rs
@@ -24,6 +24,8 @@ fn main() {
     let result = match (args.day.as_str(), args.part.as_str()) {
         ("day1", "1") => ex1_1(),
         ("day1", "2") => ex1_2(),
+        ("day2", "1") => ex2_1(),
+        ("day2", "2") => ex2_2(),
         _ => Err(format!("could not get `{} {}`", day, part)),
     };
 
@@ -32,26 +34,36 @@ fn main() {
 
 fn ex1_1() -> Result<i32, String> {
     let stdin = std::io::stdin();
-    let vec: Vec<i32> = stdin.lock().lines().map(|x| x.unwrap().parse().unwrap()).collect();
-    let (_, res) = vec.iter().fold((0, 0), |(current_depth, depth_increased), &depth| {
-        if current_depth == 0 {
-            // println!("{} (N/A - no previous measurement)", depth);
-            (depth, 0)
-        } else if depth > current_depth {
-            // println!("{} (increased)", depth);
-            (depth, depth_increased + 1)
-        } else {
-            // println!("{} (decreased)", depth);
-            (depth, depth_increased)
-        }
-    });
+    let vec: Vec<i32> = stdin
+        .lock()
+        .lines()
+        .map(|x| x.unwrap().parse().unwrap())
+        .collect();
+    let (_, res) = vec
+        .iter()
+        .fold((0, 0), |(current_depth, depth_increased), &depth| {
+            if current_depth == 0 {
+                // println!("{} (N/A - no previous measurement)", depth);
+                (depth, 0)
+            } else if depth > current_depth {
+                // println!("{} (increased)", depth);
+                (depth, depth_increased + 1)
+            } else {
+                // println!("{} (decreased)", depth);
+                (depth, depth_increased)
+            }
+        });
 
     Ok(res)
 }
 
 fn ex1_2() -> Result<i32, String> {
     let stdin = std::io::stdin();
-    let vec: Vec<i32> = stdin.lock().lines().map(|x| x.unwrap().parse().unwrap()).collect();
+    let vec: Vec<i32> = stdin
+        .lock()
+        .lines()
+        .map(|x| x.unwrap().parse().unwrap())
+        .collect();
     let (_, res) = vec
         .windows(3)
         .fold((0, 0), |(current_depth, depth_increased), depths| {
@@ -70,3 +82,63 @@ fn ex1_2() -> Result<i32, String> {
 
     Ok(res)
 }
+
+fn ex2_1() -> Result<i32, String> {
+    // let mut h_dist = 0;
+    // let mut v_dist = 0;
+
+    let stdin = std::io::stdin();
+    let (h, v) = stdin
+        .lock()
+        .lines()
+        .map(|x| ex2_parse(x.unwrap()))
+        .fold((0, 0), {
+            |(h, v), (dir, val)| match dir {
+                'f' => (h, v + val),
+                'u' => (h - val, v),
+                _ => (h + val, v),
+            }
+        });
+
+    println!("h:{},v:{} = **{}", h, v, h * v);
+
+    Ok(h * v)
+}
+
+fn ex2_2() -> Result<i32, String> {
+    let stdin = std::io::stdin();
+    let (h, v, _) = stdin
+        .lock()
+        .lines()
+        .map(|x| ex2_parse(x.unwrap()))
+        .fold((0, 0, 0), {
+            |(h, v, a), (dir, val)| match dir {
+                // down X increases your aim by X units.
+                // up X decreases your aim by X units.
+                // forward X does two things:
+                //     It increases your horizontal position by X units.
+                //     It increases your depth by your aim multiplied by X.
+                'd' => (h, v, a + val),
+                'u' => (h, v, a - val),
+                _ => (h + val, v + (val * a), a),
+            }
+        });
+
+    println!("h:{},v:{} = **{}", h, v, h * v);
+
+    Ok(h * v)
+}
+
+fn ex2_parse(line: String) -> (char, i32) {
+    let mut split = line.trim().split(" ");
+    let command = split.next().unwrap();
+    let value: i32 = split.next().unwrap().parse().unwrap();
+
+    match command {
+        "forward" => ('f', value),
+        "up" => ('u', value),
+        "down" => ('d', value),
+        _ => ('e', value),
+    }
+}
+
